39449eb8f099c4b5e16243b4358402214b895b70
a464442700 <464442700@qq.com>
2023-04-23 18:17:10 +0800

    2023-04-23-02

--- migrate_data/src/main/java/com/lxf/dao/SessionInterceptor.java
+++ migrate_data/src/main/java/com/lxf/dao/SessionInterceptor.java
@@ -1,8 +1,12 @@
 package com.lxf.dao;
 
+import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
 
+@Intercepts({@Signature(type = Executor.class, method = "query", args = {com.lxf.mapper.BFSMapper.class, Object.class})})
 public class SessionInterceptor  implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
@@ -13,6 +17,6 @@ public class SessionInterceptor  implements Interceptor {
     }
     private void callProcedure() {
         // 调用存储过程的代码
-        System.out.println();
+        System.out.println("执行拦截器");
     }
 }
